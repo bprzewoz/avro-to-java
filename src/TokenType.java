65a3d24811118b522200552c24ec15e8195e91b7
Bartlomiej Przewozniak <Bartlomiej.Przewozniak@sas.com>
2017-10-13 16:31:01 +0200

    Creating TokenType.java

---
+++ src/TokenType.java
@@ -0,0 +1,15 @@
+/**
+ * Created by splbap on 2017-10-11.
+ */
+public enum TokenType {
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    LEFT_BRACKET,
+    RIGHT_BRACKET,
+    COMMA,
+    COLON,
+    SEMICOLON,
+    STRING,
+    NUMBER,
+    LITERAL
+}
